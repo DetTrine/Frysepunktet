947f669ad2cfd461246965cee1b27a41651224a8
Trine <Trine@LAPTOP-PMSON3RL>
2026-02-12 12:00:22 +0100

    Add project files.

---
+++ Frysepunktet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+
+namespace Frysepunktet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What might the temp be");
+            float temp = float.Parse(Console.ReadLine());
+
+            if (temp == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Ideal but keep an eye out");
+            }
+            else if (temp < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Fucking freezing, damn, but that's good");
+            }
+            else if (temp > 1)
+            { 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Temperature too hot like u boo");
+            }
+            Console.ResetColor ();
+
+        }
+    }
+}
+   
